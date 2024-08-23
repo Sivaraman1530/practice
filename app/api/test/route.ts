aa9c55ac781ba320cbbd7bd819eb7eb32e6f8f62
Player <player@MacBook-Air.local>
2024-08-23 20:37:22 +0530

    ei commit

--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -1,11 +1,7 @@
 import { sql } from "@/app/db"
 
 export async function GET() {
-  const res=await sql`select * from cars`
+  const res=await sql`select * from public.health_check`
 
-  return new Response(  JSON.stringify(res), {
-    headers: { 
-      'Access-Control-Allow-Origin': 'http://localhost:3001', // Allow requests from this origin
-     },
-  })
+  return new Response(  JSON.stringify(''))
 }
--- app/db/index.ts
+++ app/db/index.ts
@@ -1,5 +1,5 @@
 import postgres from 'postgres'
-const DATABASE_URL = 'postgres://try:try@localhost:5432/player';
+const DATABASE_URL = 'postgres://default:OxDsbI7VA0zS@ep-lively-math-a1x13a86.ap-southeast-1.aws.neon.tech:5432/verceldb?sslmode=require';
 
 
 export const sql = postgres(DATABASE_URL) // will use psql environment variables
